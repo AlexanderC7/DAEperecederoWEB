6e7db9f41ad706b81fd163899845f38cbaf0493f
AlexanderC7 <117324985+AlexanderC7@users.noreply.github.com>
2025-04-02 12:54:09 -0500

    Agregado ServicioPerecedero y modificaciones en Controlador

--- src/main/java/com/servidor/supermercado/controller/PerecederoController.java
+++ src/main/java/com/servidor/supermercado/controller/PerecederoController.java
@@ -1,5 +1,7 @@
 package com.servidor.supermercado.controller;
 
+import com.servidor.supermercado.model.Perecedero;
+import com.servidor.supermercado.services.ServicioPerecedero;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/perecederos")
 public class PerecederoController {
+
+    private ServicioPerecedero servicioPerecedero;
 
     @GetMapping("/")
     public String devolverPrincipal(){
@@ -16,6 +20,13 @@ public class PerecederoController {
     @GetMapping("/healthStatus")
     public String devolverEstado(){
         return "Estado: OK";
+    }
+
+    @GetMapping("/find")
+    public String buscarPerecedero(){
+        String respuesta;
+        respuesta = servicioPerecedero.getPerecederos().toString();
+        return respuesta;
     }
 
 }
---
+++ src/main/java/com/servidor/supermercado/services/ServicioPerecedero.java
@@ -0,0 +1,13 @@
+package com.servidor.supermercado.services;
+
+import com.servidor.supermercado.model.Perecedero;
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class ServicioPerecedero {
+
+    private ArrayList<Perecedero> perecederos = new ArrayList<>();
+
+}
