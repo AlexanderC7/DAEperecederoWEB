44cd8effa122d3c615806078841891dcc256bf95
AlexanderC7 <117324985+AlexanderC7@users.noreply.github.com>
2025-04-02 13:07:03 -0500

    Cambios de prueba en Servicio y controlador

--- src/main/java/com/servidor/supermercado/controller/PerecederoController.java
+++ src/main/java/com/servidor/supermercado/controller/PerecederoController.java
@@ -2,9 +2,11 @@ package com.servidor.supermercado.controller;
 
 import com.servidor.supermercado.model.Perecedero;
 import com.servidor.supermercado.services.ServicioPerecedero;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
 @RequestMapping(value = "/perecederos")
@@ -13,8 +15,9 @@ public class PerecederoController {
     private ServicioPerecedero servicioPerecedero;
 
     @GetMapping("/")
-    public String devolverPrincipal(){
-        return "Perecederos";
+    public ResponseEntity<String> devolverPrincipal(){
+        servicioPerecedero.llenarListaPrueba();
+        return ResponseEntity.ok("Perecederos añadidos correctamente");
     }
 
     @GetMapping("/healthStatus")
--- src/main/java/com/servidor/supermercado/services/ServicioPerecedero.java
+++ src/main/java/com/servidor/supermercado/services/ServicioPerecedero.java
@@ -3,6 +3,7 @@ package com.servidor.supermercado.services;
 import com.servidor.supermercado.model.Perecedero;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Getter
@@ -10,4 +11,10 @@ public class ServicioPerecedero {
 
     private ArrayList<Perecedero> perecederos = new ArrayList<>();
 
+    public void llenarListaPrueba(){
+        Perecedero pere1 = new Perecedero("Bocadillo", 123, 2000.0, 2, LocalDateTime.now());
+        perecederos.add(pere1);
+        Perecedero pere2 = new Perecedero("Leche", 124, 9200.0, 10, LocalDateTime.now());
+        perecederos.add(pere2);
+    }
 }
